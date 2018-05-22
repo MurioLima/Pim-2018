30a879810333dd9eeef4b68c1c8043e708231999
MuriloLimaADS <36330856+MurioLima@users.noreply.github.com>
2018-05-22 15:41:23 -0300

    Add files via upload

---
+++ unip-webservice/connect.php
@@ -0,0 +1,11 @@
+<?php 
+	 define('HOST', 'localhost');
+	 define('USER','id5832541_gabe');
+	 define('PASS', '10100110');
+	 define('DBNAME', 'id5832541_unip');
+	 
+	 define('LOGIN', 'admin');
+	 define('SENHA', '123');
+	 
+	 $con = mysqli_connect(HOST,USER,PASS,DBNAME);
+?>
